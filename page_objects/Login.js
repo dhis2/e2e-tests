4c367aaf056c31fea5525b777ca35f62bfb0375d
Edoardo Sabadelli <edoardo@dhis2.org>
2017-09-28 10:06:40 +0200

    Extend page objects

--- page_objects/Login.js
+++ page_objects/Login.js
@@ -1,17 +1,24 @@
 import Page from './Page';
 
 class Login extends Page {
-    get usernameInput() { return browser.element('input#j_username') }
-    get passwordInput() { return browser.element('input[type=password]') }
+    get loginForm() { return browser.element('#loginForm') }
+    get loginMessage() { return browser.element('#loginMessage') }
+    get usernameInput() { return browser.element('#j_username') }
+    get passwordInput() { return browser.element('#j_password') }
     get submitButton() { return browser.element('input[type=submit]') }
+
+    open() {
+        super.open('dhis-web-commons/security/login.action');
+    }
 
     doLogin(username, password) {
         this.open();
+
+        browser.waitForExist(this.loginForm.selector);
+
         this.usernameInput.setValue(username);
         this.passwordInput.setValue(password);
         this.submitButton.click();
-
-        browser.pause(2000);
     }
 }
 
