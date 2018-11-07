283b9ff3294f906e2b2571e877ec94b4a09f5f59
Gintare Vilkelyte <vilkelyte.gintare@gmail.com>
2018-11-07 14:00:20 +0100

    Expressing intent: Changed parameter name in isVisible

--- features/support/check/isVisible.js
+++ features/support/check/isVisible.js
@@ -2,20 +2,20 @@
  * Check if the given element is (not) visible
  * @param  {String}   element   Element selector
  * @param  {Object}   element   WebElement JSON object
- * @param  {String}   falseCase Check for a visible or a hidden element
+ * @param  {String}   shouldNotBeVisible Pass this if element should not be visible
  */
-module.exports = (element, falseCase) => {
+module.exports = (element, shouldNotBeVisible) => {
     /**
      * Visible state of the give element
      * @type {(String|Object}
      */
     const isVisible = (typeof element === 'string') ? browser.isVisible(element) : element.isVisible();
 
-    if (falseCase) {
-        expect(isVisible).to.not
-            .equal(true, `Expected element "${element}" not to be visible`);
+    if (shouldNotBeVisible) {
+        expect(isVisible).to
+            .equal(false, `Expected element "${element}" should not have been visible`);
     } else {
         expect(isVisible).to
-            .equal(true, `Expected element "${element}" to be visible`);
+            .equal(true, `Expected element "${element}" should have been visible`);
     }
 };
