86f7eacad466b36046cb7fcaca7feddf78bea195
Gintare Vilkelyte <vilkelyte.gintare@gmail.com>
2019-06-04 10:53:05 +0200

    Escaping quotes

--- update-instance.sh
+++ update-instance.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-'''
-instance_host='[\"verify.dhis2.org\"]'
+
+instance_host="[\"verify.dhis2.org\"]"
 instance_name=$instance_name
 instance_action="reset_war"
 credentials=$AWX_BOT_CREDENTIALS
 aws_url="https://awx.dhis2.org/api/v2"
 
 job_trigger_response=$(curl -v -u $credentials \
-  -d '{\"extra_vars\":{\"instance_host\":'$instance_host',\"instance_name\": \"'$instance_name'\",\"instance_action\": \"'$instance_action'\"}}' \
+  -d "{\"extra_vars\":{\"instance_host\":$instance_host,\"instance_name\": \"$instance_name\",\"instance_action\": \"$instance_action\"}}" \
   -H "Content-Type: application/json" \
   -s https://awx.dhis2.org/api/v2/job_templates/10/launch/ | python -m json.tool)
 
@@ -27,6 +27,3 @@ do
     status=$(echo $job_response | python -c 'import sys, json; print json.load(sys.stdin)["status"]')
 
 done
-
-
-'''
