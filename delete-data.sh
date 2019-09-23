5713f1c93f6449956e1f6c7f0ad2563d1703d5c2
Gintare Vilkelyte <vilkelyte.gintare@gmail.com>
2019-09-23 15:20:08 +0200

    AUTO-47: Adding script for deleting buggy metadata [v33] (#15)
    * Adding script for deleting buggy metadata

---
+++ delete-data.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+
+url=$url
+crendentials=$credentials
+declare -a report_tables=('CurZOghc7Mh' 'jhr1eSnZeMr')
+declare -a maps=("AHWtSmx21sx" "gJ1BHisY9Wm")
+declare -a charts=("ME1zXcf4zvu" "bDhkM10HzKO")
+
+# $1- resource
+# $2 - array of uids
+function send_delete_request() {
+  resource=$1; shift
+  ids=( "$@" )
+  echo "Params: $resource, ${ids[@]}"
+
+  echo $arr
+  for id in "${ids[@]}"; do
+      echo "Deleting from $resource, uid: $id"
+      response=$(curl -u $crendentials \
+      -X DELETE \
+      -s \
+      -o /dev/null \
+      --write-out "%{http_code}" \
+      $url/api/$resource/$id )
+      echo "Status code: $response"
+    done
+}
+
+send_delete_request "reportTables" "${report_tables[@]}"
+send_delete_request "maps" "${maps[@]}"
+send_delete_request "charts" "${charts[@]}"
+
+
