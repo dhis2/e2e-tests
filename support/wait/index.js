87de3bc399e62ba88a47ad8a7c905fc0f584ea1c
Gintare Vilkelyte <vilkelyte.gintare@gmail.com>
2020-09-03 12:07:57 +0200

    feat: adds waitForClickable

--- support/wait/index.js
+++ support/wait/index.js
@@ -1,3 +1,4 @@
 export { default as waitForElementToExist } from './waitForElementToExist';
 export { default as waitForVisible } from './waitForVisible';
 export { default as waitForWindowToLoad } from './waitForWindowToLoad';
+export { default as waitForClickable } from './waitForClickable';
