5bfc160c8c745ea87940dec82dba956aa832825a
Gintare Vilkelyte <vilkelyte.gintare@gmail.com>
2020-10-30 13:56:34 +0100

    chore: fix wdio.conf webdriver conf

--- wdio.conf.js
+++ wdio.conf.js
@@ -1,6 +1,9 @@
 const chai = require('chai');
 require('@babel/register');
 const jiraService = require('./services/wdio-jira-integration-service').default;
+const drivers = {
+  chrome: { version: '86.0.4240.22' }, // https://chromedriver.chromium.org/
+}
 exports.config = {
   //
   // ====================
@@ -88,6 +91,7 @@ exports.config = {
   // Services take over a specific job you don't want to take care of. They enhance
   // your test setup with almost no effort. Unlike plugins, they don't add new
   // commands. Instead, they hook themselves up into the test process.
+  
   services: [
     [ jiraService, {
       isEnabled: true,
@@ -103,31 +107,14 @@ exports.config = {
       apiKey: process.env.APPLITOOLS_API_KEY,
       forceFullPageScreenshot: true,
     }],
-    'selenium-standalone'
+    ['selenium-standalone', {
+      logPath: 'logs',
+      installArgs: { drivers },
+      args: { drivers },
+    }]
   ],
 
   seleniumLogs: './logs',
-  //seleniumInstallArgs: { version: "3.141.59" },
-  //seleniumArgs: { version: "3.141.59" },
-  seleniumInstallArgs: {
-    version: "3.141.59",
-    drivers: {
-      chrome: {
-        version: '84.0.4147.30', // depending on when u see this, you may want something newer
-        arch: process.arch,
-        baseURL: 'https://chromedriver.storage.googleapis.com',
-      },
-    }
-  },
-  seleniumArgs: {
-    version: "3.141.59",
-    drivers: {
-      chrome: {
-        version: '84.0.4147.30',
-        arch: process.arch
-      },
-    },
-  },
 
   // Framework you want to run your specs with.
   // The following are supported: Mocha, Jasmine, and Cucumber
