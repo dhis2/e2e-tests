c723189c6f089b783f96463463c2a6bd7db1958d
Gintare Vilkelyte <vilkelyte.gintare@gmail.com>
2021-02-04 10:34:42 +0100

    fix: url for viewing event in capture (#68)

--- page_objects/capture/CaptureViewEventPage.js
+++ page_objects/capture/CaptureViewEventPage.js
@@ -2,7 +2,7 @@ import Page from '../Page'
 import { waitForWindowToLoad} from '@support/wait';
 class CaptureViewEventPage extends Page {
   open(eventId) {
-    super.open('dhis-web-capture/#/viewEvent/' + eventId);
+    super.open('dhis-web-capture/#/viewEvent?viewEventId=' + eventId);
     waitForWindowToLoad();
   }
 
