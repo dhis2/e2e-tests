51294af318fe842e7747d9c8ab6e8f17b9acce1c
Gintare Vilkelyte <vilkelyte.gintare@gmail.com>
2021-06-16 12:11:11 +0200

    fix: data visualiser getting started

--- page_objects/analytics/DataVisualiser.js
+++ page_objects/analytics/DataVisualiser.js
@@ -10,7 +10,7 @@ class DataVisualiser extends Page {
   }
 
   get gettingStartedElement() {
-    return browser.$('[data-test="start-screen"]');
+    return browser.$('[class*="StartScreen_outer"]');
   }
 
   open() {
