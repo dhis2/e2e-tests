da75dafbcbecd66b316adf20ae281dace5d2b825
Gintare Vilkelyte <vilkelyte.gintare@gmail.com>
2021-12-29 11:05:08 +0100

    fix: comment

--- support/action/getFilteredConsoleLog.js
+++ support/action/getFilteredConsoleLog.js
@@ -4,7 +4,7 @@ module.exports = () => {
   return getConsoleLog().filter((value) => {
     const message = value['message'];
 
-    // excluding possible errors that is not considered as errors, but rather as backend functionality
+    // excluding errors that are either known issues or expected under certain db states
     return !(message.includes('status of 404') &&
       (message.includes('userDataStore/') ||
       message.includes('staticContent/logo_banner') || //https://jira.dhis2.org/browse/DHIS2-7656
