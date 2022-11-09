c70f6a3223919126c803323de727b97e527ebe7d
radnov <radnov@pm.me>
2022-11-09 21:28:56 +0200

    feat: Add generate-analytics script [DEVOPS-152]

---
+++ scripts/generate-analytics.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+instance_url="$1"
+
+default_dhis2_credentials="admin:district"
+
+function instance_response() {
+  $HTTP --follow --headers get "$instance_url" | head -1 | cut -d ' ' -f 2
+}
+
+instance_readiness_start_time=$SECONDS
+until [[ "$(instance_response)" == "200" ]]
+do
+  echo "Instance not ready yet, $(($SECONDS-$instance_readiness_start_time))s elapsed ..."
+  sleep 30
+done
+echo "Instance is ready! Triggering Analytics generation ..."
+
+analytics_status_endpoint=$(
+  $HTTP --auth "$default_dhis2_credentials" post "$instance_url/api/resourceTables/analytics" |
+  jq -r '.response .relativeNotifierEndpoint'
+)
+echo "Analytics notifier: $analytics_status_endpoint"
+
+function analytics_status() {
+  $HTTP --auth "$default_dhis2_credentials" get "${instance_url}${analytics_status_endpoint}" |
+  jq -r '.[] .completed'
+}
+
+analytics_tasks_readiness_start_time=$SECONDS
+until [[ "$(analytics_status)" =~ "true" ]]
+do
+  echo "Analytics tasks haven't completed yet, $(($SECONDS-$analytics_tasks_readiness_start_time))s elapsed ..."
+  sleep 30
+done
+echo "Analytics tasks have completed!"
