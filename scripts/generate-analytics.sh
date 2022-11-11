75e359f7a939726116690721d6a7dfcf090eac4e
radnov <radnov@pm.me>
2022-11-11 15:11:29 +0200

    refactor: Add a readiness threshold for the generate-analytics script

--- scripts/generate-analytics.sh
+++ scripts/generate-analytics.sh
@@ -9,11 +9,24 @@ function instance_response() {
   $HTTP --follow --headers get "$instance_url" | head -1 | cut -d ' ' -f 2
 }
 
+function analytics_status() {
+  $HTTP --auth "$dhis2_credentials" get "${instance_url}${analytics_status_endpoint}" |
+  jq -r '.[] .completed'
+}
+
+instance_readiness_threshold=300
 instance_readiness_start_time=$SECONDS
 until [[ "$(instance_response)" == "200" ]]
 do
-  echo "Instance not ready yet, $(($SECONDS-$instance_readiness_start_time))s elapsed ..."
-  sleep 30
+  elapsed_seconds=$(($SECONDS-$instance_readiness_start_time))
+
+  if [[ $elapsed_seconds -gt $instance_readiness_threshold ]]; then
+    echo "Instance didn't get ready in the ${instance_readiness_threshold}s threshold."
+    exit 1
+  fi
+
+  echo "Instance not ready yet, ${elapsed_seconds}s elapsed ..."
+  sleep 10
 done
 echo "Instance is ready! Triggering Analytics generation ..."
 
@@ -23,15 +36,10 @@ analytics_status_endpoint=$(
 )
 echo "Analytics notifier: $analytics_status_endpoint"
 
-function analytics_status() {
-  $HTTP --auth "$dhis2_credentials" get "${instance_url}${analytics_status_endpoint}" |
-  jq -r '.[] .completed'
-}
-
 analytics_tasks_readiness_start_time=$SECONDS
 until [[ "$(analytics_status)" =~ "true" ]]
 do
   echo "Analytics tasks haven't completed yet, $(($SECONDS-$analytics_tasks_readiness_start_time))s elapsed ..."
-  sleep 30
+  sleep 10
 done
 echo "Analytics tasks have completed!"
