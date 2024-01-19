a361f072c90233c2afda0643249938a30d2ef61c
Adel Bensaad <adel@dhis2.org>
2024-01-19 10:00:58 +0000

    chore: add checks for undefined variables (#313)
    and apply code formatting in Cypress tests

--- cypress/e2e/capture.cy.js
+++ cypress/e2e/capture.cy.js
@@ -1,61 +1,63 @@
 /// <reference types="cypress" />
 
-import { openApp, 
+import {
+  openApp,
   fillEventForm,
   addComment,
   openEvent,
   Selectors,
   ContextActions,
   getCommentByValue,
-} from '../utils/capture'
+} from "../utils/capture";
 
-import { getCurrentUserDisplayName } from '../utils/api'
+import { getCurrentUserDisplayName } from "../utils/api";
 
-describe('Capture', () => {
+describe("Capture", () => {
   beforeEach(() => {
     openApp();
-  })
+  });
 
-  it('should open new event form', () => {
-    ContextActions.selectProgramByName('Information Campaign')
-    ContextActions.selectOrgUnitByName('Ngelehun CHC')
-    cy
-      .get(Selectors.NEW_EVENT_BUTTON)
+  it("should open new event form", () => {
+    ContextActions.selectProgramByName("Information Campaign");
+    ContextActions.selectOrgUnitByName("Ngelehun CHC");
+    cy.get(Selectors.NEW_EVENT_BUTTON)
       .click()
       .get(Selectors.NEW_EVENT_IN_SELECTED_PROGRAM_BUTTON)
-      .click()
-  
-    cy.get(Selectors.NEW_EVENT_FORM).should('be.visible')
-  })
-  
-  it('should create event', () => {
-    const comment = "Test comment";
-    cy.visit('dhis-web-capture/index.html#/new?orgUnitId=DiszpKrYNg8&programId=q04UBOqq3rp');
-    cy.intercept('POST','**/tracker*').as('post');
-
-    fillEventForm();
-    addComment( comment)
-    
-    cy.get(Selectors.SAVE_BUTTON)
       .click();
 
-    cy.wait('@post').then((interception) => {
-      cy.log(interception.response.body.bundleReport.typeReportMap['EVENT'])
-        cy.wrap(interception.response.body.bundleReport.typeReportMap['EVENT'].objectReports[0].uid).as('eventId');
-        
-    })
-    
-    cy.location('href').should('not.contain', 'new');
-    cy.get(Selectors.WORKING_LIST_TABLE)
-      .should('be.visible');
-    cy.get('@eventId').then((eventId) => {
-      openEvent(eventId)
-    })
+    cy.get(Selectors.NEW_EVENT_FORM).should("be.visible");
+  });
 
-    getCurrentUserDisplayName().then( displayName => {
+  it("should create event", () => {
+    const comment = "Test comment";
+    cy.visit(
+      "dhis-web-capture/index.html#/new?orgUnitId=DiszpKrYNg8&programId=q04UBOqq3rp"
+    );
+    cy.intercept("POST", "**/tracker*").as("post");
+
+    fillEventForm();
+    addComment(comment);
+
+    cy.get(Selectors.SAVE_BUTTON).click();
+
+    cy.wait("@post").then((interception) => {
+      cy.log(interception.response.body.bundleReport.typeReportMap["EVENT"]);
+      cy.wrap(
+        interception.response.body.bundleReport.typeReportMap["EVENT"]
+          .objectReports[0].uid
+      ).as("eventId");
+    });
+
+    cy.location("href").should("not.contain", "new");
+    cy.get(Selectors.WORKING_LIST_TABLE).should("be.visible");
+    cy.get("@eventId").then((eventId) => {
+      openEvent(eventId);
+    });
+
+    getCurrentUserDisplayName().then((displayName) => {
       getCommentByValue(comment)
-      .get('[data-test=comment-user]' )
-      .should('have.text', displayName)
-    })
-  })
-})
+        .get("[data-test=comment-user]")
+        .should("have.text", displayName);
+    });
+  });
+});
