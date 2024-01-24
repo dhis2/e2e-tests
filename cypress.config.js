a7feab066867bb4286e7e9a790511168799b7d6a
Adel Bensaad <adel@dhis2.org>
2024-01-24 11:09:30 +0000

    debug

--- cypress.config.js
+++ cypress.config.js
@@ -21,7 +21,10 @@ module.exports = defineConfig({
       console.log(`Current Working Directory: ${process.cwd()}`);
 
       // Read dynamic environment variables
-      const envPath = path.resolve(process.cwd(), "../../cypress.env.json");
+      const envPath = path.resolve(
+        "/home/ubuntu/jenkins-tmp/workspace/e2e-tests_PR-318",
+        "cypress.env.json"
+      );
       console.log(`Resolved Path for cypress.env.json: ${envPath}`);
 
       if (fs.existsSync(envPath)) {
