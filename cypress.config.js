6a0f2e48152d9eaa366447edb4f108b7759f82f2
Adel Bensaad <adel@dhis2.org>
2024-01-24 07:25:36 +0000

    Fix path resolution for cypress.env.json in cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -21,8 +21,10 @@ module.exports = defineConfig({
       console.log(`Current Working Directory: ${process.cwd()}`);
 
       // Read dynamic environment variables
-      const envPath = path.resolve(__dirname, "cypress.env.json");
-      //const envPath = path.resolve(process.cwd(), "cypress.env.json");
+      const envPath = path.resolve(
+        `../../${process.cwd()}`,
+        "cypress.env.json"
+      );
       console.log(`Resolved Path for cypress.env.json: ${envPath}`);
 
       if (fs.existsSync(envPath)) {
