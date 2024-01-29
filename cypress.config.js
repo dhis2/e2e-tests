3eff977ce7f90491c7cf79bbe3b7172c79fc3398
Adel Bensaad <adel@dhis2.org>
2024-01-29 15:30:14 +0000

    Refactor init-data logic into a separate Node.js script [v39] (#319)
    * Refactor init-data logic into a separate Node.js script (#318)
    
    * Refactor init-data logic into a separate Node.js script for sequential initialization
    
    * install axios
    
    * Update Cypress config and initDataScript for env var handling
    
    * ci
    
    * debug
    
    * debug
    
    * fix
    
    * cleaning and debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * delete the precreated cypress.env.json
    
    * debug : use absolute path for archiving
    
    * debug
    
    * debug ci use stash
    
    * debug ci
    
    * debug ci
    
    * debug ci
    
    * ci
    
    * debugging dynamic env variables loading
    
    * Fix path resolution for cypress.env.json in cypress.config.js
    
    * debug
    
    * debug path resolution for cypress.env.json in cypress.config.js
    
    * debug
    
    * Isolate Cypress Env Config
    
    * cleanup PR
    
    * chore: use yarn

--- cypress.config.js
+++ cypress.config.js
@@ -1,28 +1,36 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
+const fs = require("fs");
+const path = require("path");
 
 module.exports = defineConfig({
   requestTimeout: 20000,
   defaultCommandTimeout: 20000,
   chromeWebSecurity: false,
   video: false,
-  projectId: 'dhis2-e2e',
+  projectId: "dhis2-e2e",
   env: {
-    LOGIN_USERNAME: 'admin',
-    LOGIN_PASSWORD: 'district',
-    allure: 'true',
+    LOGIN_USERNAME: "admin",
+    LOGIN_PASSWORD: "district",
+    allure: "true",
     grepTags: process.env.TAGS,
-    allureResultsPath: 'reports/allure-results'
+    allureResultsPath: "reports/allure-results",
   },
   numTestsKeptInMemory: 0,
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      require('cypress-grep/src/plugin')(config);
-      return require('./cypress/plugins/index.js')(on, config)
+      // Read dynamic environment variables
+      const envPath = path.resolve("/e2e/env_files", "cypress.env.json");
+
+      if (fs.existsSync(envPath)) {
+        const dynamicEnv = JSON.parse(fs.readFileSync(envPath, "utf8"));
+        config.env = { ...config.env, ...dynamicEnv };
+      }
+
+      require("cypress-grep/src/plugin")(config);
+      return require("./cypress/plugins/index.js")(on, config);
     },
-    baseUrl: 'https://smoke.dhis2.org/dev_smoke',
-    specPattern: './cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    baseUrl: "https://smoke.dhis2.org/dev_smoke",
+    specPattern: "./cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
     experimentalSessionAndOrigin: true,
   },
-})
+});
