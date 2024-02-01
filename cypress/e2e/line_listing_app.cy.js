1b40ed62cca599a6a62fdfe04c4c5d727450b00a
Adel Bensaad <adel@dhis2.org>
2024-02-01 10:24:35 +0000

    skip line-listing-app tests on v37 due to feature unavailability

--- cypress/e2e/line_listing_app.cy.js
+++ cypress/e2e/line_listing_app.cy.js
@@ -3,35 +3,12 @@ import { LINE_LISTING_APP, loadLineList } from "../utils/analytics";
 describe("Line listing -> DHIS2-13221", { tags: ["smoke"] }, () => {
   const lineLists = Cypress.env("eventVisualizations");
 
-  beforeEach(() => {
-    cy.clearConsoleLogs();
-    cy.visit(LINE_LISTING_APP);
-  });
-
-  // Check if 'lineLists' is defined and is an array
-  if (!Array.isArray(lineLists) || lineLists.length === 0) {
-    it("No line lists defined in Cypress environment", () => {
+  before(() => {
+    if (!Array.isArray(lineLists) || lineLists.length === 0) {
       cy.log(
-        "Skipping tests because no line lists are defined in Cypress environment"
+        "Line listing app feature tests are skipped for this DHIS2 version (V37) as the feature is not developed yet."
       );
-    });
-  } else {
-    lineLists.forEach((lineList) => {
-      it(lineList.displayName, () => {
-        loadLineList(lineList.id);
-
-        cy.getConsoleLogs().should((logs) => {
-          const reportLog =
-            "Line list: " +
-            lineList.displayName +
-            " has " +
-            logs.length +
-            " severe errors: \n" +
-            JSON.stringify(logs, null, 1);
-
-          expect(logs, reportLog).to.have.length(0);
-        });
-      });
-    });
-  }
+      this.skip();
+    }
+  });
 });
