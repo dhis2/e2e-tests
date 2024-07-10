88349b22e80d659d6e2bf48ccf3548d77ca453df
Adel Bensaad <adel@dhis2.org>
2024-07-10 09:21:54 +0100

    Revert "optimise waitForResources function to improve resource loading efficiency"
    This reverts commit eef1cca91153db81e6e7287f16829d24079d0a5d.

--- cypress/support/commands/wait.js
+++ cypress/support/commands/wait.js
@@ -1,20 +1,41 @@
 import "cypress-wait-until";
 
-Cypress.Commands.add("waitForResources", (interval = 1000, timeout = 30000) => {
-  cy.log("waiting for resources");
+Cypress.Commands.add(
+  "waitForResources",
+  (interval = 1000, timeout = 30000, retry = 5) => {
+    cy.log("waiting for resources");
 
-  const start = Date.now();
-  const checkResources = () => {
-    const now = Date.now();
-    const entries = cy.state("window").performance.getEntriesByType("resource");
+    var retries = retry;
+    const getCount = () => {
+      const entries = cy
+        .state("window")
+        .performance.getEntriesByType("resource");
 
-    if (now - start > timeout || entries.length === 0) {
-      cy.log("finished waiting");
-      return true;
-    }
+      return entries.length;
+    };
 
-    cy.wait(interval).then(checkResources);
-  };
+    var count = getCount();
 
-  checkResources();
-});
+    const checkNoNewLong = () => {
+      const newCount = getCount();
+
+      if (newCount == count) {
+        if (retries <= 0) {
+          cy.log("finished waiting");
+          return true;
+        }
+        retries--;
+        return false;
+      } else {
+        retries = retry;
+        count = newCount;
+        return false;
+      }
+    };
+
+    cy.waitUntil(() => Promise.resolve(checkNoNewLong()), {
+      interval: interval,
+      timeout: timeout,
+    });
+  }
+);
