5bddab45e81611d1545f21392204d6e0937ed218
Adel Bensaad <adel@dhis2.org>
2024-07-10 14:25:10 +0100

    fix async/sync mix-up in new waitForResources command

--- cypress/support/commands/wait.js
+++ cypress/support/commands/wait.js
@@ -5,27 +5,30 @@ Cypress.Commands.add("waitForResources", (interval = 3000, timeout = 30000) => {
 
   const start = Date.now();
   let previousLength = 0;
+
   const checkResources = () => {
-    const now = Date.now();
-    const entries = cy.state("window").performance.getEntriesByType("resource");
-    const currentLength = entries.length;
+    return cy.window().then((win) => {
+      const now = Date.now();
+      const entries = win.performance.getEntriesByType("resource");
+      const currentLength = entries.length;
 
-    cy.log(`Time elapsed: ${now - start}ms`);
-    cy.log(`Resource entries: ${currentLength}`);
+      cy.log(`Time elapsed: ${now - start}ms`);
+      cy.log(`Resource entries: ${currentLength}`);
 
-    if (now - start > timeout) {
-      cy.log("Timeout reached, stopping waitForResources");
-      return true;
-    }
+      if (now - start > timeout) {
+        cy.log("Timeout reached, stopping waitForResources");
+        return true;
+      }
 
-    if (currentLength === previousLength) {
-      cy.log("No new resources loaded, stopping waitForResources");
-      return true;
-    }
+      if (currentLength === previousLength) {
+        cy.log("No new resources loaded, stopping waitForResources");
+        return true;
+      }
 
-    previousLength = currentLength;
-    cy.wait(interval).then(checkResources);
+      previousLength = currentLength;
+      return cy.wait(interval).then(checkResources);
+    });
   };
 
-  checkResources();
+  return checkResources();
 });
