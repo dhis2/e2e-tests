eef1cca91153db81e6e7287f16829d24079d0a5d
Adel Bensaad <adel@dhis2.org>
2024-07-10 08:39:51 +0100

    optimise waitForResources function to improve resource loading efficiency

--- cypress/support/commands/wait.js
+++ cypress/support/commands/wait.js
@@ -1,41 +1,20 @@
 import "cypress-wait-until";
 
-Cypress.Commands.add(
-  "waitForResources",
-  (interval = 1000, timeout = 30000, retry = 5) => {
-    cy.log("waiting for resources");
+Cypress.Commands.add("waitForResources", (interval = 1000, timeout = 30000) => {
+  cy.log("waiting for resources");
 
-    var retries = retry;
-    const getCount = () => {
-      const entries = cy
-        .state("window")
-        .performance.getEntriesByType("resource");
+  const start = Date.now();
+  const checkResources = () => {
+    const now = Date.now();
+    const entries = cy.state("window").performance.getEntriesByType("resource");
 
-      return entries.length;
-    };
+    if (now - start > timeout || entries.length === 0) {
+      cy.log("finished waiting");
+      return true;
+    }
 
-    var count = getCount();
+    cy.wait(interval).then(checkResources);
+  };
 
-    const checkNoNewLong = () => {
-      const newCount = getCount();
-
-      if (newCount == count) {
-        if (retries <= 0) {
-          cy.log("finished waiting");
-          return true;
-        }
-        retries--;
-        return false;
-      } else {
-        retries = retry;
-        count = newCount;
-        return false;
-      }
-    };
-
-    cy.waitUntil(() => Promise.resolve(checkNoNewLong()), {
-      interval: interval,
-      timeout: timeout,
-    });
-  }
-);
+  checkResources();
+});
