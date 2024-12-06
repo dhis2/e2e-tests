4879f5869c455f042e5b83974b3a6d67845d17ef
rohit <rohit@dhis2.org>
2024-12-06 20:41:56 +0530

    added android settings web apps tests

--- cypress/e2e/android_web_settings.cy.js
+++ cypress/e2e/android_web_settings.cy.js
@@ -3,6 +3,7 @@ describe("android", () => {
     // Visit a URL
     cy.visit("/api/apps/android-settings-app/index.html#/");
 
+    //Start of General settings
     // Navigate to General Settings
     cy.get('[data-test="sidebar-link-general-settings"]').click();
 
@@ -30,12 +31,27 @@ describe("android", () => {
     cy.get('[data-test="dhis2-uicore-buttonstrip"] > :nth-child(1) > [data-test="dhis2-uicore-button"]')      
       .should("be.disabled");
 
+    //ANDROAPP-3048: General-Reserved Values - cannot enter negative number
+    cy.contains("Reserved values downloaded per TEI attribute")
+      .parent()
+      .find('input')
+      .get('input[type="number"]')
+      .last()
+      .clear()
+      .type('-10')
+      .should('have.value', '10');
+
     //ANDROAPP-3930: General - Reset all values to default
     // Reset default settings and save the changes
     cy.get(':nth-child(2) > [data-test="dhis2-uicore-button"]').click()
     cy.get('[data-test="dhis2-uicore-buttonstrip"] > :nth-child(1) > [data-test="dhis2-uicore-button"]')      
       .should("be.enabled");
-
+    cy.get(':nth-child(6) > [data-test="dhis2-uiwidgets-checkboxfield"] > [data-test="dhis2-uiwidgets-checkboxfield-content"] > [data-test="dhis2-uicore-checkbox"] > .icon')
+      .should("not.be.checked");
+    cy.contains("Reserved values downloaded per TEI attribute")
+      .parent()
+      .find('input')
+      .should('have.value', '100');
 
     // Navigate to Global Settings
     cy.get(
@@ -52,12 +68,15 @@ describe("android", () => {
     cy.get('[data-test="sidebar-link-dataset-appearance"] > a.jsx-2002348738 > .jsx-2002348738').click();
     cy.get('.Button_container__padding__emSUv > :nth-child(1) > [data-test="dhis2-uicore-button"]').should("be.disabled");
 
-//Test code
+    //End of General settings
 
-    // Interact with elements
-    //      cy.get('[data-test="element-selector"]').click();
-    //      cy.get('[data-test="input-selector"]').type("Test data");
+    //Start of Synchronisation settings
 
+    //ANDROAPP-3053: Synchronization - Programs - Global - Download TEI with enrollment date within
+
+    //End of Synchronisation settings
+
+    //Start of Global settings
     cy.get('[data-test="sidebar-link-global-settings"]').click();
 
     // check if Tracker Import and Export checkboxes are checked
@@ -89,30 +108,6 @@ describe("android", () => {
 
     // Find the dropdown with the label "How often should metadata sync?"
 
-    //cy.get('[data-test="output-selector"]').should("contain", "How often should metadata sync?");
-    //    cy.contains("How often should metadata sync?");
-
-    /*cy.contains('label', 'How often should metadata sync?')
-  .parent() // Navigate to the parent container of the label
-  .within(() => {
-    // Open the dropdown
-    cy.get('[data-test="dhis2-uicore-select-input"]').click();
-    //cy.get('[data-test="dhis2-uicore-select-menu-menuwrapper"]').should('be.visible')
-    cy.contains('1 Day').should('exist');
-
-    //cy.contains('1 Week').should('exist');
-    //cy.contains('Manual').should('exist');
-
-    // Assert that the dropdown menu contains the three options
-    const expectedOptions = ['24h', '7d', 'Manual'];
-    expectedOptions.forEach((option) => {
-      cy.get('[data-test="dhis2-uicore-select-menu-menuwrapper"]').contains(option).should('exist');
-    });
-
-    // Close the dropdown (optional)
-    cy.get('[data-test="dhis2-uicore-select-input"]').click();
-    */
-  });
 });
 
-//});
+});
